c7e71b39437f7037f50e7af1777045be4ad3653d
Hiupii <nguyentrhieu2312@gmail.com>
2024-09-10 12:29:03 +0700

    change gpio for esp32-cam, add pwm to motor

--- firmware/mini_drone/main/mini_drone.c
+++ firmware/mini_drone/main/mini_drone.c
@@ -12,8 +12,8 @@
 /*----------mpu6050 var----------*/
 #define MPU_ADDR (0x68)
 
-#define CONFIG_SDA_GPIO GPIO_NUM_21
-#define CONFIG_SCL_GPIO GPIO_NUM_22
+#define CONFIG_SDA_GPIO GPIO_NUM_13
+#define CONFIG_SCL_GPIO GPIO_NUM_12
 
 static const char *MPU_TAG = "mpu6050_log";
 
@@ -33,9 +33,9 @@ pid_t pitch_pid;
 struct pid_controller roll_control;
 pid_t roll_pid;
 
-float Kp = 2.75;   // Proportional param
+float Kp = 3.55;   // Proportional param
 float Ki = 0.005;   // Integral param
-float Kd = 1.65;   // Derivative param
+float Kd = 2.05;   // Derivative param
 
 static float output_pitch = 0.0f;
 static float output_roll = 0.0f;
@@ -44,10 +44,10 @@ static float setpoint_roll = 0.0f;
 
 
 /*----------motor var----------*/
-#define MOT_1 GPIO_NUM_32
-#define MOT_2 GPIO_NUM_33
-#define MOT_3 GPIO_NUM_26
-#define MOT_4 GPIO_NUM_27
+#define MOT_1 GPIO_NUM_14
+#define MOT_2 GPIO_NUM_15
+#define MOT_3 GPIO_NUM_2
+#define MOT_4 GPIO_NUM_4
 
 int base_throttle = 4915;
 int motor1_pwm = 0;
@@ -85,8 +85,38 @@ void mpu6050_task(void *pvParameters)
         ESP_ERROR_CHECK(mpu6050_get_motion(&dev, &accel, &rotation));
         input_pitch = asinf(accel.x * g / g) * 180 / M_PI;
         input_roll = atan2(accel.y, accel.z) * 180 / M_PI;
-        // ESP_LOGI(MPU_TAG, "Pitch: %f", input_pitch);
-        // ESP_LOGI(MPU_TAG, "Roll : %f", input_roll);
+        ESP_LOGI(MPU_TAG, "Pitch: %f", input_pitch);
+        ESP_LOGI(MPU_TAG, "Roll : %f", input_roll);
+
+        pid_compute(pitch_pid);
+        pid_compute(roll_pid);
+
+        // ESP_LOGI(PID_TAG, "Input Pitch: %f, Output Pitch: %f", input_pitch, output_pitch);
+        // ESP_LOGI(PID_TAG, "Input Roll : %f, Output Roll : %f", input_roll, output_roll);
+
+        motor1_pwm = base_throttle + output_pitch - output_roll;
+        motor2_pwm = base_throttle + output_pitch + output_roll;
+        motor3_pwm = base_throttle - output_pitch + output_roll;
+        motor4_pwm = base_throttle - output_pitch - output_roll;
+
+        motor1_pwm = constrain(motor1_pwm, 0, 8191);  // 13-bit PWM max value is 8191
+        motor2_pwm = constrain(motor2_pwm, 0, 8191);
+        motor3_pwm = constrain(motor3_pwm, 0, 8191);
+        motor4_pwm = constrain(motor4_pwm, 0, 8191);
+
+        // ESP_LOGI(PID_TAG, "mot1_pwm: %d", motor1_pwm-base_throttle);
+        // ESP_LOGI(PID_TAG, "mot2_pwm: %d", motor2_pwm-base_throttle);
+        // ESP_LOGI(PID_TAG, "mot3_pwm: %d", motor3_pwm-base_throttle);
+        // ESP_LOGI(PID_TAG, "mot4_pwm: %d", motor4_pwm-base_throttle);
+
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, motor1_pwm);
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, motor2_pwm);
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, motor3_pwm);
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, motor4_pwm);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3);
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
@@ -137,51 +167,41 @@ void setup_pwm() {
     }
 }
 
-void pid_task(void *pvParameters)
-{
+// void pid_task(void *pvParameters)
+// {
+//     while(1)
+//     {
 
-    pid_sample(pitch_pid, 100);
-    pid_sample(roll_pid, 100);
-    while(1)
-    {
-        pid_compute(pitch_pid);
-        pid_compute(roll_pid);
-
-        ESP_LOGI(PID_TAG, "Input Pitch: %f, Output Pitch: %f", input_pitch, output_pitch);
-        ESP_LOGI(PID_TAG, "Input Roll : %f, Output Roll : %f", input_roll, output_roll);
-
-        motor1_pwm = base_throttle + output_pitch - output_roll;
-        motor2_pwm = base_throttle + output_pitch + output_roll;
-        motor3_pwm = base_throttle - output_pitch + output_roll;
-        motor4_pwm = base_throttle - output_pitch - output_roll;
-
-        motor1_pwm = constrain(motor1_pwm, 0, 8191);  // 13-bit PWM max value is 8191
-        motor2_pwm = constrain(motor2_pwm, 0, 8191);
-        motor3_pwm = constrain(motor3_pwm, 0, 8191);
-        motor4_pwm = constrain(motor4_pwm, 0, 8191);
-
-        // ESP_LOGI(PID_TAG, "mot1_pwm: %d", motor1_pwm);
-        // ESP_LOGI(PID_TAG, "mot2_pwm: %d", motor2_pwm);
-        // ESP_LOGI(PID_TAG, "mot3_pwm: %d", motor3_pwm);
-        // ESP_LOGI(PID_TAG, "mot4_pwm: %d", motor4_pwm);
-
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
+//     }
+// }
 
 void app_main()
 {
+    setup_pwm();
+
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, base_throttle);
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, base_throttle);
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, base_throttle);
+    ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, base_throttle);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2);
+    ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3);
+
     // task
     ESP_ERROR_CHECK(i2cdev_init());
 
     pitch_pid = pid_create(&pitch_control, &input_pitch, &output_pitch, &setpoint_pitch, Kd, Ki, Kp);
     roll_pid = pid_create(&roll_control, &input_roll, &output_roll, &setpoint_roll, Kp, Ki, Kd);
 
+    pid_limits(pitch_pid, -8191, 8191);
+    pid_limits(roll_pid, -8191, 8191);
+
     pid_auto(pitch_pid);
     pid_auto(roll_pid);
 
     xTaskCreate(mpu6050_task, "mpu6050_test", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
-    xTaskCreate(pid_task, "pid_task", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
+    // xTaskCreate(pid_task, "pid_task", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
 }
 
 // Hàm constrain: giới hạn giá trị trong phạm vi min và max
